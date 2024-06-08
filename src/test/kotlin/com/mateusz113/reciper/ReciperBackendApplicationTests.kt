369da2396aa2ad69ccb59a3ef15125649e6fe097
Mateusz113 <mateusz.ch.113@gmail.com>
2024-06-08 17:44:57 +0200

    Intiial repository and project setup

---
+++ src/test/kotlin/com/mateusz113/reciper/ReciperBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mateusz113.reciper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReciperBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
